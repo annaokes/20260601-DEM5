f52362674961c8ed294477ec0419a11d0e653fc0
annaokes <test@test.com>
2026-06-03 15:29:49 +0100

    commented out sqlal

--- app/docker/main.py
+++ app/docker/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import datetime as dt
-from sqlalchemy import create_engine
+#from sqlalchemy import create_engine
 from config import TODAY, BOOKS_PATH, CUSTOMERS_PATH, BOOKS_TABLE_NAME, CUSTOMERS_TABLE_NAME, CONNECTION_STRING, SAVE_TO_SQL
 from loguru import logger
 
