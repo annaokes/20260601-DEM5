b74b3d8b8617ee82584fd31d17f0d57e2d5deda9
annaokes <test@test.com>
2026-06-03 14:00:15 +0100

    added files

---
+++ docker/calculator.py
@@ -0,0 +1,6 @@
+def calculator(num1, num2):
+    return num1/num2
+
+x = calculator(144,12)
+
+print(f'Hello, 144 divided by 12 is {x}')
---
+++ docker/volume.py
@@ -0,0 +1,10 @@
+from pathlib import Path 
+from datetime import datetime
+import os
+
+Path("/data/cleaned_data").mkdir(parents=True, exist_ok=True)
+
+out = Path("/data/cleaned_data/hello.txt")
+
+with out.open("a") as f:
+    f.write(f"Hello docker volume: {datetime.now()}")
